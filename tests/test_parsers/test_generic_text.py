449a3bc7c8451963ab3cbde22fec1e779e74c88d
Corey Oordt <coreyoordt@gmail.com>
2023-07-19 08:47:48 -0500

    Removed an invalid test

--- tests/test_parsers/test_generic_text.py
+++ tests/test_parsers/test_generic_text.py
@@ -22,33 +22,6 @@ from archeion.parsers.generic_text import parse_links_from_path
                 "http://dodson-pham.com/category.html",
                 "https://www.berg-cruz.com/list/tags/app/post.html",
             ],
-            id="url_list.txt",
-        )
-    ],
-)
-def test_parse_txt_file(file_path: str, expected_links: list, fixture_dir: Path):
-    """The parse_url_list function should return a list of URLs."""
-    urltext = fixture_dir.joinpath(file_path).read_text()
-    urls = parse_links_from_path(urltext)
-    assert isinstance(urls, list)
-    assert set(urls) == set(expected_links)
-
-
-@pytest.mark.parametrize(
-    "file_path, expected_links",
-    [
-        param(
-            "url-list.txt",
-            [
-                "https://farrell-turner.info/",
-                "https://cooper-christian.com/#fragment",
-                "https://dalton.com/?q=1&b=56",
-                "http://cox.com/tag/posts/search.html",
-                "https://gross-melton.com/",
-                "https://www.miller-mckenzie.org/author/",
-                "http://dodson-pham.com/category.html",
-                "https://www.berg-cruz.com/list/tags/app/post.html",
-            ],
             id="full-path-url_list.txt",
         )
     ],
@@ -56,6 +29,6 @@ def test_parse_txt_file(file_path: str, expected_links: list, fixture_dir: Path)
 def test_parse_txt_file_filepath(file_path: str, expected_links: list, fixture_dir: Path):
     """If a file path is passed as the string, it should read it and parse it."""
     fullpath = fixture_dir.joinpath(file_path)
-    urls = parse_links_from_path(str(fullpath))
+    urls = parse_links_from_path(fullpath)
     assert isinstance(urls, list)
     assert set(urls) == set(expected_links)
